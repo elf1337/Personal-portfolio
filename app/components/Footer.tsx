dc5b397010b8160322fb7cbdfdf724b1c94d8d6e
elf1337 <akhil.1337s@gmail.com>
2023-12-15 11:57:21 +0530

    Fix font size

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <section className='py-12 bg-[#1F2130]'>
       <div className="container mx-auto">
-        <div className='flex justify-center  items-center text-white font-light'>
+        <div className='flex justify-center  items-center text-white font-light text-sm'>
           Copyright &copy; Akhil S. All Right reserved.
         </div>
       </div>
